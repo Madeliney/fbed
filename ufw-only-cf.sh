8cdd9f99925da546b4475c872cd4a77adb484b6a
Madeliney <76511305+Madeliney@users.noreply.github.com>
2023-03-20 20:04:57 +0800

    Update ufw-only-cf.sh

--- ufw-only-cf.sh
+++ ufw-only-cf.sh
@@ -4,10 +4,12 @@ for ipv4 in `curl -s https://www.cloudflare.com/ips-v4 | tee ips-v4`
 do
     sudo ufw allow from $ipv4 to any port 80
     sudo ufw allow from $ipv4 to any port 443
+    sudo ufw allow from $ipv4 to any port 8443
 done
 
 for ipv6 in `curl -s https://www.cloudflare.com/ips-v6 | tee ips-v6`
 do
     sudo ufw allow from $ipv6 to any port 80
     sudo ufw allow from $ipv6 to any port 443
+    sudo ufw allow from $ipv6 to any port 8443
 done
